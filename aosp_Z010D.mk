a3f533d1c44ad883cb5ec3a5b137a0785266c125
SakilMondal <mondalsakil@gmail.com>
2018-11-23 13:27:23 +0530

    Z010D: AEX-ify
    *Let's do this as AOSP way.

---
+++ aosp_Z010D.mk
@@ -0,0 +1,19 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from Z010D device
+$(call inherit-product, device/asus/Z010D/device.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/asus/msm8916-common/overlay/lineage
+
+PRODUCT_NAME := aosp_Z010D
+PRODUCT_DEVICE := Z010D
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,3 @@
-add_lunch_combo dot_Z010D-userdebug
+add_lunch_combo aosp_Z010D-eng
+add_lunch_combo aosp_Z010D-user
+add_lunch_combo aosp_Z010D-userdebug
